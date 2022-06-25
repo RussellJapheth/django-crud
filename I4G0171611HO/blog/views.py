fe00b63aa8f203a4e4f1593b8cf5e525aae1d596
Russell Japheth <japheth.russell.i@gmail.com>
2022-06-25 13:49:46 +0100

    Here be dragons

---
+++ I4G0171611HO/blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+
+from .models import Post
+# Create your views here.
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
